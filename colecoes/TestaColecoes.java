ccd942d8899f86f3249c528d0db486f1a0635017
Unknown <lidia.g.arantes@gmail.com>
2018-10-04 22:15:08 -0300

    classe colecoes

---
+++ colecoes/TestaColecoes.java
@@ -0,0 +1,14 @@
+package colecoes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestaColecoes {
+	
+	public static void main(String[] args) {
+		//List
+		List<String> nomes;
+		
+		nomes = new ArrayList<>();
+	}
+}
